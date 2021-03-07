61258419411674db954cde87d4d3bcc2abfce487
kammy0603 <wingzero0127@gmail.com>
2021-03-07 20:31:30 +0900

    TeamDB更新

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,23 @@
 class TeamsController < ApplicationController
   def top
   end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:teamname, :prefecture_id, :year, :age_id, :frequency_id, :level_id, :offer_id, :achievement, :location, :teamimage)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_085307) do
+ActiveRecord::Schema.define(version: 2021_03_07_112645) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,22 @@ ActiveRecord::Schema.define(version: 2021_03_07_085307) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "teamname", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "year", null: false
+    t.integer "age_id", null: false
+    t.integer "frequency_id", null: false
+    t.integer "level_id", null: false
+    t.integer "offer_id", null: false
+    t.string "achievement", null: false
+    t.string "location", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_teams_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -56,4 +72,5 @@ ActiveRecord::Schema.define(version: 2021_03_07_085307) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "teams", "users"
 end
